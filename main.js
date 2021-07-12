4bc42620f7841bd94dac164624a3cdb777985092
1 <tawers6103@gmail.com>
2021-07-12 14:38:12 +0300

    js

---
+++ main.js
@@ -0,0 +1,72 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let ninehondred = 999;
+let onetwothree = 123;
+let p = 3.14;
+let twosw = 2.7;
+let sixt = 16;
+let truth = true;
+let falsee = false;
+console.log(hello,owu,com,ua)
+//alert(one)
+document.write(`<h1>${p}</h1>`)
+document.write(`<h2>${twosw}</h2>`)
+document.write(`<p>${sixt}</p>`)
+console.log(truth)
+console.log(falsee)
+
+sixt = 16.5;
+hello = 'bye';
+owu = 'okten';
+com = 'casasadad';
+ua = 'ru';
+one = 2;
+ten = 20;
+ninehondred = 1000;
+onetwothree = 321;
+p = 2.14;
+twosw = 2.5;
+truth = false;
+falsee = true;
+console.log(hello,owu,com,ua)
+alert(one)
+document.write(`<h1>${p}</h1>`)
+document.write(`<h2>${twosw}</h2>`)
+document.write(`<p>${sixt}</p>`)
+console.log(truth)
+console.log(falsee)
+let firstName = ' Євген'
+let middleName = ' Іванович'
+let lastName = ' Василик'
+let person = lastName+firstName+middleName
+let age = 17;
+
+
+let prom = prompt('Імя')
+let prom2 = prompt('фамілія')
+let prom3 = prompt('роки')
+console.log(prom,prom2,prom3)
+
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 === 6);
+console.log(10 <= 10);
+console.log(10 >= 10);
+console.log( 10 !== 10);
+console.log(10 != 10);
+console.log(10 !== 10);
+console.log(123 === '123');
+console.log(123 == '123');
